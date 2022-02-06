4615bb7e459216a59b44ba330a7e81c9c32374f1
Vahagn <vahagn_harutyunyan@mail.ru>
2022-02-06 20:39:10 +0400

    Arajin shnorhavoranqnery

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Shnorhavor amanor ev surb tsnund");
+        System.out.println("Avelacnenq evs shnorhavoranq");
     }
 }
