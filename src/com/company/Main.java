8f6d05283067feb1aff09ab2b89bea114c83582e
Vahagn <vahagn_harutyunyan@mail.ru>
2022-02-07 14:57:52 +0400

    Commit nor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Shnorhavor amanor ev surb tsnund");
-        System.out.println("Avelacnenq evs shnorhavoranq");
+        System.out.println("07.02.2022");
     }
 }
