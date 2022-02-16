f22fce739dae6fc368b272bde9f984d55791f926
Vahagn <vahagn_harutyunyan@mail.ru>
2022-02-17 01:13:09 +0400

    Day12 01:12

--- src/Day9/Day12.java
+++ src/Day9/Day12.java
@@ -1,5 +1,7 @@
 package Day9;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,16 +9,27 @@ public class Day12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // int x = scanner.nextInt();
+//        int[] array = new int[10];
+//        Random rand = new Random();
+//        for (int i = 0; i < 10; i++) {
+//            array[i] = -100 + rand.nextInt(200);
+//        }
 
 
 //        exerciseOne(x); //1.
 //        exerciseTwo(x); //2.
-        int[] array = new int[10]; //3.
 //        numberOfPositive(array); //3.
 //        exerciseFour(); //4.
 //        adjacentElement(array); //5.
-        largestAndIndex();//6
-
+//        largestAndIndex();//6
+//        smallestPositive(); //7
+//        different(); //8
+//        reverseWithoutChange(); //9.
+//        reverseRearrange(); //10.
+//        adjacentElementsRearrange(); //11.
+//        cycle(); //12.
+//        swapMinMax(); //13.
+//        arrayWithIndexK(array, 4);
 
     }
 
@@ -105,15 +118,142 @@ public class Day12 {
         }
     }
 
-    /**
-     * 6. An array of numbers is given. Print the value of the largest element in the array,
-     * and then the index of that element in the array. If there are several largest elements,
-     * print the index of the first of them.
-     * Example` Input` 1 2 3 2 1
-     * Output` The largest is 3    index is 2
-     */
     static void largestAndIndex() {
-        int[] arr = {1, 2, 3, 5, 4, 22, 33, -10, 2, -20};
+        int[] arr = {1, 2, 3, 5, 4, 22, 33, -10, 50, -20};
+        int max = arr[0];
+        int maxIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("max: " + max + "\t\t" + "maxIndex: " + maxIndex);
 
+    }
+
+    static void smallestPositive() {
+        int[] arr = {-2, 5, 4, -6, 8, 20, 10, 2};
+        int small = arr[0];
+        int i = 0;
+        for (; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                small = arr[i];
+                break;
+            }
+        }
+        for (; i < arr.length; i++) {
+            if (arr[i] > 0 && arr[i] < small) {
+                small = arr[i];
+            }
+        }
+        System.out.println("smallest positive: " + small);
+    }
+
+    static void different() {
+        int[] arr = {1, 1, 1, 2, 2, 2, 2, 3, 8, 11, 11, 11};
+        int count = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                System.out.println(arr[i - 1] + " -> " + arr[i]);
+                count++;
+            }
+        }
+        System.out.println("count: " + count);
+    }
+
+    static void reverseWithoutChange() {
+        int[] arr = {1, 1, 1, 2, 2, 2, 2, 3, 8, 11, 11, 11};
+        int size = arr.length - 1;
+        for (int i = size; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void reverseRearrange() {
+        int[] arr = {1, 2, 34, 5, 6, 7, 8, 9, -12};
+        int size = arr.length - 1;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[size];
+            arr[size] = temp;
+            size--;
+        }
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+    }
+
+
+    static void adjacentElementsRearrange() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+    }
+
+    static void cycle() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+    }
+
+    static void swapMinMax() {
+        int[] arr = {1, 2, 3, 14, 5, -3, 7};
+        int min = arr[0];
+        int max = arr[0];
+        int indexMin = 0;
+        int indexMax = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                indexMin = i;
+            } else if (arr[i] > max) {
+                max = arr[i];
+                indexMax = i;
+            }
+        }
+        System.out.println("min: " + min + "\t\tmax: " + max);
+        int temp = arr[indexMin];
+        arr[indexMin] = arr[indexMax];
+        arr[indexMax] = temp;
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+
+    }
+
+    static void arrayWithIndexK(int[] arr, int index) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("Invalid");
+            return;
+        }
+
+        int k = arr[index];
+        for (int p : arr) {
+            System.out.print(p + " ");
+        }
+        System.out.println("\n" + arr[index]);
+        for (int i = index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = Integer.MAX_VALUE;
+        for (int p : arr) {
+            System.out.print(p + " ");
+        }
     }
 }
